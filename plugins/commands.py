690f211b9d935ab652b0f90faba5f26b653d4851
theKunalSingh143 <120199298+theKunalSingh143@users.noreply.github.com>
2023-10-17 09:17:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -58,8 +58,6 @@ async def start(client, message):
                     InlineKeyboardButton('〄 Hᴇʟᴘ', callback_data='help'),
                     InlineKeyboardButton('⍟ Aʙᴏᴜᴛ', callback_data='about')
                 ],[
-                  InlineKeyboardButton('💌 SOURCE CODE 💌', callback_data='source')
-                ],[
                     InlineKeyboardButton('✇ Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ✇', url=CHNL_LNK)
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)

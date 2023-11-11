eaca62814b8babd36fc281a585f969015e6109b0
theKunalSingh143 <120199298+theKunalSingh143@users.noreply.github.com>
2023-11-11 10:30:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1863,23 +1863,6 @@ InlineKeyboardButton('💰 Buy Premium For Ad Free Movies', callback_data='buy_p
                 reply_markup=reply_markup,
                 parse_mode=enums.ParseMode.HTML
             )
-        elif query.data == "plan_premium":
-            btn = [[
-                    InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("💳 Buy Premium", url="https://t.me/IllegalDeveloperBot")  
-                  ]]
-        
-            await client.edit_message_media(
-                query.message.chat.id, 
-                query.message.id, 
-                InputMediaPhoto("https://graph.org/file/758a0089b5aec87f6fa8f.jpg")
-            )
-            reply_markup = InlineKeyboardMarkup(btn)
-            await query.message.edit_text(
-                text=(script.PLAN_PREMIUM),
-                reply_markup=reply_markup,
-                parse_mode=enums.ParseMode.HTML
-            )
     elif query.data == "buy_premium":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),

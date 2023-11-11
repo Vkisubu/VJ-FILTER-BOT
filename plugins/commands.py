b19995ffe0f5808c2a3d882b456b245d22719fe9
theKunalSingh143 <120199298+theKunalSingh143@users.noreply.github.com>
2023-11-11 08:49:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -82,8 +82,8 @@ async def start(client, message):
             return
         btn = [
             [
-                InlineKeyboardButton("❆ Jᴏɪɴ Cʜᴀɴɴᴇʟ 1 ❆", url=f"https://t.me/+_N4GYhmbBcVmZGQ9")
-                InlineKeyboardButton("❆ Jᴏɪɴ Cʜᴀɴɴᴇʟ 2 ❆", url=invite_link.invite_link)
+                InlineKeyboardButton("❆ Jᴏɪɴ Cʜᴀɴɴᴇʟ", url=f"https://t.me/+6PFpU3wYyYMyZWU1")
+                InlineKeyboardButton("❆ Jᴏɪɴ Cʜᴀɴɴᴇʟ", url=invite_link.invite_link)
             ],[
                 InlineKeyboardButton('🤔 Why Iam Join🤔', callback_data='sinfo')
             ]

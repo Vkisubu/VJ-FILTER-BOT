ece3fdeda44e4fd86e8adeecdc9849d2ad7f9822
theKunalSingh143 <120199298+theKunalSingh143@users.noreply.github.com>
2023-11-29 10:18:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -253,7 +253,7 @@ async def start(client, message):
         is_valid = await check_token(client, userid, token)
         if is_valid == True:
             madie = InlineKeyboardMarkup([
-                [InlineKeyboardButton("Verify Successful, Get Your File", url=f"https://t.me/+4RB2-U2o9yE4ZmQ9")],
+                [InlineKeyboardButton("Verify Successful, Get Your File", url=f"f"https://telegram.me/{temp.U_NAME}?start=files_{fileid}")],
             ])
             await message.reply_text(
                 text=f"<b>Hey {message.from_user.mention}\n\nNow You are successfully verified For Today ✓\n\nNow You have Unlimited Access For All Movies Till Today Midnight.</b>",
